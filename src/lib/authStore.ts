77b1bfaf2d7c1f78c95bc708a5870c12b90504e2
kinfe-michael <kmichael3d@gmail.com>
2025-07-30 10:52:05 +0300

    auth stote name corrected

--- src/lib/authStore.ts
+++ src/lib/authStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface User {
   userId: string;
@@ -58,7 +58,7 @@ export const useAuthStore = create<AuthState>()(
   },
 }),
  {
-      name: 'cart-data', 
+      name: 'auth-data', 
       storage: createJSONStorage(() => localStorage), 
     }
    )
